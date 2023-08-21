6e19992efacb0d9b43223f4caa62109e829e6d87
EricoBis <ericobis.santos@gmail.com>
2023-08-21 19:10:50 -0300

    added | CollectionFactory class

---
+++ CollectionFactory.java
@@ -0,0 +1,9 @@
+import java.util.Collection;
+
+public class CollectionFactory {
+    
+    public static <E> Collection<E> criaInstancia(int tam, boolean ehOrdenado, boolean podeRepetir){
+        //TODO 
+        return null;
+    }
+}

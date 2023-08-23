c0387b6bf43b9e08019323b2a96b423540e53319
EricoBis <ericobis.santos@gmail.com>
2023-08-23 10:50:36 -0300

    move | CollectionFactory movido para a pasta Exercicio1

--- CollectionFactory.java
+++
@@ -1,9 +0,0 @@
-import java.util.Collection;
-
-public class CollectionFactory {
-    
-    public static <E> Collection<E> criaInstancia(int tam, boolean ehOrdenado, boolean podeRepetir){
-        //TODO 
-        return null;
-    }
-}
--- Exercicio1/App.java
+++ Exercicio1/App.java
@@ -1,5 +1,6 @@
+package Exercicio1;
 public class App{
     public static void main(String[] args) {
-        
+    
     }
 }
